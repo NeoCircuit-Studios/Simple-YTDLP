660fb974823c44e65451e94f64789c78886034be
NeoCircuit <154694006+NeoCircuit-Studios@users.noreply.github.com>
2025-08-13 14:02:16 +0200

    Update mainscripty.tmp.cmd

--- NeoCircuit-Studios/mainscripty.tmp.cmd
+++ NeoCircuit-Studios/mainscripty.tmp.cmd
@@ -7,10 +7,10 @@
 setlocal enabledelayedexpansion
 
 
-set "YTDLPappLINK=https://raw.githubusercontent.com/NeoCircuit-Studios/Simple-YTDLP/main/NeoCircuit-Studios/tools/yt-dlp.exe"
-set "ffmpegappLINK=https://raw.githubusercontent.com/NeoCircuit-Studios/Simple-YTDLP/main/NeoCircuit-Studios/tools/ffmpeg.exe"
-set "ffplayappLink=https://raw.githubusercontent.com/NeoCircuit-Studios/Simple-YTDLP/main/NeoCircuit-Studios/tools/ffplay.exe"
-set "ffprobeappLink=https://raw.githubusercontent.com/NeoCircuit-Studios/Simple-YTDLP/main/NeoCircuit-Studios/tools/ffprobe.exe"
+set "YTDLPappLINK=https://raw.githubusercontent.com/NeoCircuit-Studios/Simple-YTDLP/main/NeoCircuit-Studios/tools/bin.6.tmp.zip"
+set "ffmpegappLINK=https://raw.githubusercontent.com/NeoCircuit-Studios/Simple-YTDLP/main/NeoCircuit-Studios/tools/bin.1.tmp.zip"
+set "ffplayappLink=https://raw.githubusercontent.com/NeoCircuit-Studios/Simple-YTDLP/main/NeoCircuit-Studios/tools/bin.2.tmp.zip"
+set "ffprobeappLink=https://raw.githubusercontent.com/NeoCircuit-Studios/Simple-YTDLP/main/NeoCircuit-Studios/tools/bin.3.tmp.zip"
 
 set "LICENSE_yt_dlplink=https://raw.githubusercontent.com/NeoCircuit-Studios/Simple-YTDLP/main/NeoCircuit-Studios/tools/LICENSE-yt-dlp.txt"
 set "LICENSE_ffmpeglink=https://raw.githubusercontent.com/NeoCircuit-Studios/Simple-YTDLP/main/NeoCircuit-Studios/tools/LICENSE-ffmpeg.txt"
@@ -27,43 +27,33 @@ echo         2025
 echo @@@@@@@@@@@@@@@@@@@@
 echo.
 
-echo "Loading.."
+echo Loading..
 echo.
 
-echo "Checking for updates..."
+echo Checking for updates...
 
 cd ..
 
-::~/..
+:: dir [ROOT]
 
-del /f /q "Simple-YTDLP.cmd" 
+curl -L "%updatelink%" -o "Simple-YTDLP.cmd"
 
 if exist "Simple-YTDLP.cmd" (
-)
-else (
     echo.
-    echo "ERROR 20"
-    echo.
-    exit /b 1
-)
-
-curl -s -o "Simple-YTDLP.cmd" "%updatelink%"
-if exist "Simple-YTDLP.cmd" (
-    echo.
-    echo "Update complete..."
+    echo Update complete...
     echo.
 ) else (
     echo.
-    echo "Failed to update Simple-YTDLP.cmd"
-    echo "ERROR 2"
-    exit /b 1
+    echo Failed to update Simple-YTDLP.cmd
+    echo ERROR 2
+    pause
 )
 
 cd "NeoCircuit-Studios"
 
-::dir [root]/NeoCircuit-Studios/
+::[root]/NeoCircuit-Studios/
 
-echo "Checking for tools..."
+echo Checking for tools Packs...
 
 if not exist "tools" (
     mkdir "tools"
@@ -71,94 +61,117 @@ if not exist "tools" (
 
 cd "tools"
 
-::dir [root]/NeoCircuit-Studios/tools/
+:: [root]/NeoCircuit-Studios/tools/
 
 if not exist "yt-dlp.exe" (
     echo.
-    echo "Downloading yt-dlp..."
-    echo.
-    curl -L -o "yt-dlp.exe" "%YTDLPappLINK%"
+    echo Downloading yt-dlp...
+    echo.   
+    curl -L "%YTDLPappLINK%" -o "bin.6.tmp.zip"
 ) 
 
 if not exist "ffmpeg.exe" (
     echo.
-    echo "Downloading ffmpeg..."
+    echo Downloading ffmpeg...
     echo.
-    curl -L -o "ffmpeg.exe" "%ffmpegappLINK%"
+    curl -L "%ffmpegappLINK%" -o "bin.1.tmp.zip"
 )
 
 if not exist "ffplay.exe" (
     echo.
-    echo "Downloading ffplay..."
+    echo Downloading ffplay...
     echo.
-    curl -L -o "ffplay.exe" "%ffplayappLink%"
+    curl -L "%ffplayappLink%" -o "bin.2.tmp.zip"
 ) 
 
 if not exist "ffprobe.exe" (
     echo.
-    echo "Downloading ffprobe..."
+    echo Downloading ffprobe...
     echo.
-    curl -L -o "ffprobe.exe" "%ffprobeappLink%"
+    curl -L "%ffprobeappLink%" -o "bin.3.tmp.zip"
 )
 
 if not exist "LICENSE-yt-dlp.txt" (
     echo.
-    echo "Downloading LICENSE-yt-dlp.txt..."
+    echo Downloading LICENSE_1
     echo.
-    curl -L -o "LICENSE-yt-dlp.txt" "%LICENSE_yt_dlplink%"
+    curl -L "%LICENSE_yt_dlplink%" -o "LICENSE-yt-dlp.txt"
 )
 
 if not exist "LICENSE-ffmpeg.txt" (
     echo.
-    echo "Downloading LICENSE-ffmpeg.txt..."
+    echo Downloading LICENSE_2
     echo.
-    curl -L -o "LICENSE-ffmpeg.txt" "%LICENSE_ffmpeglink%"
+    curl -L "%LICENSE_ffmpeglink%" -o "LICENSE-ffmpeg.txt" 
 )
 
 echo.
 
-echo "Validating All Tools..."
+echo Validating All Tools...
 echo.
 timeout /t 2 >nul
 
+if exist "bin.1.tmp.zip" (
+    echo 5%
+    tar -xf "bin.1.tmp.zip"
+    del /f "bin.1.tmp.zip"
+)
+
+if exist "bin.2.tmp.zip" (
+    echo 6%
+    tar -xf "bin.2.tmp.zip"
+    del /f "bin.2.tmp.zip"
+)
+
+if exist "bin.3.tmp.zip" (
+    echo 7%
+    tar -xf "bin.3.tmp.zip"
+    del /f "bin.3.tmp.zip"
+)
+
+if exist "bin.6.tmp.zip" (
+    echo 8%
+    tar -xf "bin.6.tmp.zip"
+    del /f "bin.6.tmp.zip"
+)
 
 if exist "yt-dlp.exe" (
-    echo "10%"
+    echo 10%
 )
 else (
-    echo "yt-dlp.exe not found!"
-    echo "ERROR 3"
-    exit /b 1
+    echo yt-dlp.exe not found!
+    echo ERROR 3
+    pause
 )
 
 if exist "ffmpeg.exe" (
-    echo "40%"
+    echo 40%
 )
 else (
-    echo "ffmpeg.exe not found!"
-    echo "ERROR 4"
-    exit /b 1
+    echo ffmpeg.exe not found!
+    echo ERROR 4
+    pause
 )
 
 if exist "ffplay.exe" (
-    echo "80%"
+    echo 80%
 )
 else (
-    echo "ffplay.exe not found!"
-    echo "ERROR 5"
-    exit /b 1
+    echo ffplay.exe not found!
+    echo ERROR 5
+    pause
 )
 
 if exist "ffprobe.exe" (
-    echo "90%"
+    echo 90%
 )
 else (
-    echo "ffprobe.exe not found!"
-    echo "ERROR 6"
-    exit /b 1
+    echo ffprobe.exe not found!
+    echo ERROR 6
+    pause
 )
 
-echo "100%"
+echo 100%
 
 echo.
 
@@ -170,32 +183,40 @@ if exist "%outputdir%" (
 mkdir "%outputdir%"
 
 
-echo "All tools are downloaded and ready to use..."
+echo All tools are downloaded and ready to use...
 echo.
 timeout /t 2 >nul
-:: clear the shitty screen
+:: clear the very nice echo's (not really)
 cls
 
 color 2
 
+:retry_input
 set /p usrlink="Enter Your Public YouTube Playlist Link: "
+set "usrlink=%usrlink:"=%"
+::If someone pastes a YouTube link in quotes, yt-dlp might misinterpret it. that would suck
 
 pause
 
 echo.
 
+if "%usrlink%"=="" (
+    echo oy.. No playlist link provided!
+    goto retry_input
+)
+
 set /p mp3="Do you want to download the playlist as MP3? (y/n): (if no then it will be MP4)"
 
 if /i "%mp3%"=="y" (
     echo.
-    echo "Downloading playlist as MP3..."
+    echo Downloading playlist as MP3...
     echo.
-    start "" "yt-dlp.exe" -x --audio-format mp3 "%usrlink%" -o "%outputdir%/%(title)s.%(ext)s"
+    yt-dlp.exe -x --audio-format mp3 "%usrlink%" -o "%outputdir%/%(title)s.%(ext)s"
 ) else (
     echo.
-    echo "Downloading playlist as MP4..."
+    echo Downloading playlist as MP4...
     echo.
-    start "" "yt-dlp.exe" "%usrlink%" -o "%outputdir%/%(title)s.%(ext)s"
+    yt-dlp.exe "%usrlink%" -o "%outputdir%/%(title)s.%(ext)s"
 )
 
 exit /b 0
